44581ce0b1225b5ac65ab060a6164faf6f976140
wyller <wyllerxd@gmail.com>
2016-06-11 21:49:48 -0300

    message system

---
+++ Assets/Scripts/GUI/scrMSG_Alert.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scrMSG_Alert : MonoBehaviour {
+	private Canvas c;
+	public GameObject msgBox;
+	public Vector3 msgBoxPos;
+	//public Image[] icons = new Image[2];
+	[SerializeField]private Sprite iconsCheck;
+	[SerializeField]private Sprite iconsAlert;
+	[SerializeField]private Sprite iconsError;
+
+	void Awake(){
+		c = GetComponent<Canvas>();
+	}
+
+	void Update () {
+		if(Input.anyKeyDown){
+			DrawMsgBox("escreva um testo", 0);
+		}
+	}
+
+
+	public void DrawMsgBox(string msg, int type){
+		GameObject instancia = Instantiate(msgBox) as GameObject;
+		instancia.transform.SetParent(c.transform);
+
+		switch (type){
+		case 0:
+			//normal
+			instancia.GetComponent<Image>().color = new Color(0.23f,0.78f,0.17f,1);
+			instancia.transform.FindChild("Icone").GetComponent<Image>().sprite = iconsCheck;
+			break;
+
+		case 1:
+			//warning
+			instancia.GetComponent<Image>().color = new Color(0.78f,0.39f,0.17f,1);
+			instancia.transform.FindChild("Icone").GetComponent<Image>().sprite = iconsAlert;
+			break;
+
+		case 2:
+			//erro
+			instancia.GetComponent<Image>().color = new Color(0.78f,0.17f,0.17f,1);
+			instancia.transform.FindChild("Icone").GetComponent<Image>().sprite = iconsError;
+			break;
+
+		}
+		instancia.GetComponentInChildren<Text>().text = msg;
+		msgBoxPos = new Vector3(0,c.GetComponent<RectTransform>().rect.height, 0);
+		instancia.GetComponent<RectTransform>().position = msgBoxPos;
+	}
+}
---
+++ Assets/Scripts/GUI/scrMSG_autoDestruction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrMSG_autoDestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject.Destroy(this.gameObject,3);
+
+	}
+
+}
