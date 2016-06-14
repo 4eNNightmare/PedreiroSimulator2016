4e4dd75999e8bb884d2bf09b0c7b9dd766ad2621
Wender Silva <wenderversaofull@hotmail.com>
2016-06-13 21:03:27 -0300

    Broken Objects

---
+++ Assets/Scripts/ObjectsBehaviour/BrokenObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrokenObject : MonoBehaviour {
+
+    public GameObject breakBreaked;
+    private float velocityImpact;
+    public float MaxImpactForce;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocityImpact = this.GetComponent<Rigidbody>().velocity.magnitude;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            velocityImpact = +col.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+            Debug.Log(velocityImpact);
+        }
+        if (velocityImpact > MaxImpactForce)
+        {
+            Instantiate(breakBreaked, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
