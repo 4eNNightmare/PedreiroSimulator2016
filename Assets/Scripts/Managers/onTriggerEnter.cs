3e68aec6b5ce3d51587b6e385e3dab2ed4ff08c5
wyller <wyllerxd@gmail.com>
2016-06-15 00:41:39 -0300

    OnTriggerExit_OK !! :dancer:

---
+++ Assets/Scripts/Managers/onTriggerEnter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onTriggerEnter : MonoBehaviour {
+
+	void OnTriggerExit(Collider other){
+		if(scrPlayerEquipments.botaDireita && scrPlayerEquipments.botaEsquerda 
+		&& scrPlayerEquipments.capacete && scrPlayerEquipments.luvaDireita 
+		&& scrPlayerEquipments.luvaEsquerda){
+			GameObject.Find ("Canvas").GetComponent<scrMSG_Alert> ().DrawMsgBox ("O paulo é tipo ruim e tem problema na cama", 0);
+		}
+		else{
+			GameObject.Find ("Canvas").GetComponent<scrMSG_Alert> ().DrawMsgBox ("Vai ser burro, quer morrer desgraça, volta la e equipa essa porra", 2);
+		}
+	}
+}
--- Assets/Scripts/ObjectsBehaviour/BrokenObject.cs
+++ Assets/Scripts/ObjectsBehaviour/BrokenObject.cs
@@ -11,7 +11,7 @@ public class BrokenObject : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate() {
         velocityImpact = this.GetComponent<Rigidbody>().velocity.magnitude;
     }
 
