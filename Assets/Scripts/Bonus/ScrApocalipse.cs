c3d819e25672277d64fc0feeb3ac79899b3b883c
Wender Silva <wenderversaofull@hotmail.com>
2016-06-15 09:56:38 -0300

    Easter Egg Levolution

---
+++ Assets/Scripts/Bonus/ScrApocalipse.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrApocalipse : MonoBehaviour {
+
+    public GameObject predios;
+    public GameObject Particulas;
+    public Material ButtonOver;
+    public Material ButtonNormal;
+
+    void OnMouseOver()
+    {
+        GameObject.Find("BotaoRed").GetComponent<Renderer>().material = ButtonOver;
+    }
+    void OnMouseExit()
+    {
+        GameObject.Find("BotaoRed").GetComponent<Renderer>().material = ButtonNormal;
+    }
+    void OnMouseDown()
+    {
+        predios.GetComponent<Animator>().SetTrigger("Apocalipse");
+        Instantiate(Particulas, predios.transform.position, predios.transform.rotation);
+        Destroy(this);
+    }
+    //void Update() {
+    //    if (Input.GetMouseButtonDown(0)) {
+    //        predios.GetComponent<Animator>().SetTrigger("Apocalipse");
+    //        Instantiate(Particulas, predios.transform.position, predios.transform.rotation);
+    //    }
+    //}
+
+}
